4613675aff26a72b7fc1e98f785043ba20f7659d
kyusulamita <sulamitamorales@yahoo.com>
2018-03-22 17:55:54 -0400

    deploying latest changes

---
+++ src/common/components/Resume.js
@@ -0,0 +1,29 @@
+// import React, { Component } from 'react';
+// import { Document } from 'react-pdf/dist/entry.noworker';
+
+// class Resume extends Component{
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       numPages: null,
+//       page: 1
+//     };
+//     this.onDocumentLoad = this.onDocumentLoad.bind(this);
+//   }
+
+//   onDocumentLoad({ numPages }){
+//     this.setState({ numPages });
+//   }
+
+//   render(){
+//     const { page, numPages} = this.state;
+//     return (
+//       <div>
+//       HI
+//       </div>
+//     )
+//   }
+// }
+
+
+// export default Resume;
